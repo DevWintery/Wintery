4120d75b2675005ba9d5415d7e8454904420e108
DevWintery <dev.wintery@gmail.com>
2025-11-29 19:34:56 +0900

    fix domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://wintery-blog.vercel.app' // Replace with your actual domain
+    const baseUrl = 'https://wintery-dev.vercel.app'
 
     return {
         rules: {
