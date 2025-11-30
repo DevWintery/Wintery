8f590850dabab6fefd9f823f7eec1e4c44ac0167
DevWintery <dev.wintery@gmail.com>
2025-11-30 21:25:49 +0900

    change fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import "highlight.js/styles/atom-one-dark.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const jetbrainsMono = JetBrains_Mono({
+    subsets: ["latin"],
+    variable: "--font-mono",
+});
 
 export const metadata: Metadata = {
     title: "Wintery",
@@ -19,7 +22,15 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <head>
+                <link
+                    rel="stylesheet"
+                    as="style"
+                    crossOrigin="anonymous"
+                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+                />
+            </head>
+            <body className={`${jetbrainsMono.variable} font-sans`}>
                 <Header />
                 {children}
                 <Footer />
